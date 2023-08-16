94c7c97492d4d68331b6ec1dd58b313b7d00a28a
Kun Lin <kun.lin.596@gmail.com>
2023-08-16 21:39:05 +0900

    Add homework 1 revision

--- src/ch2/motion.cc
+++ src/ch2/motion.cc
@@ -31,13 +31,14 @@ int main(int argc, char** argv) {
     double angular_velocity_rad = FLAGS_angular_velocity * sad::math::kDEG2RAD;  // 弧度制角速度
     SE3 pose;                                                                    // TWB表示的位姿
     Vec3d omega(0, 0, angular_velocity_rad);                                     // 角速度矢量
+    Vec3d a_gravity(0, 0, -9.8);                                                 // 重力加速度(世界坐标系)
     Vec3d v_body(FLAGS_linear_velocity, 0, 0);                                   // 本体系速度
     const double dt = 0.05;                                                      // 每次更新的时间
 
     while (ui.ShouldQuit() == false) {
         // 更新自身位置
         Vec3d v_world = pose.so3() * v_body;
-        pose.translation() += v_world * dt;
+        pose.translation() += v_world * dt + 0.5 * a_gravity * dt * dt;
 
         // 更新自身旋转
         if (FLAGS_use_quaternion) {
